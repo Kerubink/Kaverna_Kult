b5b7a77250d02505c4b84960db1da74d15d9b609
Kauã Kelvyn <ckaua504@gmail.com>
2024-12-03 10:45:29 -0300

    feat: adicionando estrutura base do projeto
    criando projeto com vite react + ts
    instalando e configurando o tailwind
    configurando o shadcn/ui

---
+++ Kaverna_Kult/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <section className="w-dvw	h-dvh flex justify-center items-center">
+        <Button>clique aqui</Button>
+      </section>
+    </>
+  );
+}
+
+export default App;
