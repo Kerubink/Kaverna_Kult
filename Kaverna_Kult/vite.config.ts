3c1bedce01cf16a8f0985b0f71ea0f242f79ae6d
Kauã Kelvyn <ckaua504@gmail.com>
2025-02-03 14:55:26 -0300

    sad

--- Kaverna_Kult/vite.config.ts
+++ Kaverna_Kult/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,11 +9,6 @@ export default defineConfig({
   },
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './certs/cert.pem')),
-    },
-    host: true, 
     proxy: {
       '/api/frete': {
         target: 'https://freteapi.herokuapp.com',
